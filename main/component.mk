87cb6446e69849b3f1e97352bb42755a5ce2839b
t.schmidt@md-network.de <t.schmidt@md-network.de>
2021-04-09 01:56:19 +0200

    adding additional folders and build config files

--- main/component.mk
+++ main/component.mk
@@ -8,7 +8,3 @@ COMPONENT_EMBED_TXTFILES += certs/prvtkey.pem
 
 
 COMPONENT_EMBED_FILES := www/index.html.gz
-# COMPONENT_EMBED_FILES += www/style.css.gz 
-# COMPONENT_EMBED_FILES += www/main.js.gz
-# COMPONENT_EMBED_FILES += www/favicon.ico.gz 
-# COMPONENT_EMBED_FILES += www/jquery.slim.min.js.gz
